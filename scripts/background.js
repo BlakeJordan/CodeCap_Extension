ba8662414d2ddb60b4cd153ad4f3b3c160d890b4
Seth <sethpeterson@ku.edu>
2020-02-19 19:26:42 -0600

    Connects ocr-lambda-handler.js to background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,9 @@
-// File path constants
-const INITIAL_POPUP_FILE_PATH   = "../html/initial-state-popup.html"
-const SECONDARY_POPUP_FILE_PATH = "../html/secondary-state-popup.html"
+///////////////////////////////////////////////////////////////////////////////
+//                                 Constants                                 //
+///////////////////////////////////////////////////////////////////////////////
+const NOTIFY_OCR_EXECUTED_MESSAGE = "notify_ocr_executed";
+const SECONDARY_POPUP_FILE_PATH   = "../html/secondary-popup.html";
+
 
 
 /* Messages that trigger actions that must be done by background.js because most chrome APIs can't be used by content scripts.
@@ -9,28 +12,49 @@ const SECONDARY_POPUP_FILE_PATH = "../html/secondary-state-popup.html"
    to send messages from content scripts. */
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    console.log("Request message: " + request.message);
+    console.log("background recieved message: \"" + request.message + "\"");
 
     // Switch on message.
     switch(request.message) {
-      // Remove popup:
-      case "REMOVE_POPUP":
-        chrome.browserAction.setPopup({ popup: "" });
-        break;
 
-      // Add initial state popup:
-      case "ADD_INITIAL_POPUP":
-        chrome.browserAction.setPopup({ popup: INITIAL_POPUP_FILE_PATH });
+      // Lambda function returned.
+      case NOTIFY_OCR_EXECUTED_MESSAGE:
+        onOcrExecuted(parseInt(request.statusCode), request.text);
         break;
-
-      // Add secondary state popup:
-      case "ADD_SECONDARY_POPUP":
-        chrome.browserAction.setPopup({ popup: SECONDARY_POPUP_FILE_PATH });          // TODO: implement secondary popup
-        break;
-
-      // Show Chrome notification:
-      case "SHOW_TEXT_READY_NOTIFICATION":
-        console.log('TODO: implement text is ready Chrome notification.')
     }
+
+    // Notify sender that message was recieved.
+    sendResponse(request.message)
   }
 );
+
+// Executes when the OCR Lambda funciton returns.
+function onOcrExecuted(statusCode, recognizedText) {
+  // Switch on server-side status codes.
+
+  switch(statusCode) {
+    // Executed with no errors.
+    case 200:
+      chrome.browserAction.setPopup({ popup: SECONDARY_POPUP_FILE_PATH });    // Set to secondary popup.
+      createNotification("Your text is ready to view!");                      // Notify user that text is ready.
+      break;
+
+    // Base64 encoding error occured.
+    case 400:
+      break;
+
+    // Tesseract error occured.
+    case 500:
+      break;
+  }
+}
+
+// Creates a Chrome notification with the specified message.
+function createNotification(message) {
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "media/codecap_logo.png",
+    title: "CodeCap",
+    message: message
+  });
+}
