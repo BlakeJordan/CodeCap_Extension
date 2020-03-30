b5622d2368550235ef7e83ee5f83834f5c976712
moezeid <m.zeid97@ku.edu>
2020-03-30 16:12:03 -0500

    hey

--- scripts/background.js
+++ scripts/background.js
@@ -3,7 +3,54 @@
 ///////////////////////////////////////////////////////////////////////////////
 const NOTIFY_OCR_EXECUTED_MESSAGE = "notify_ocr_executed";
 const SECONDARY_POPUP_FILE_PATH   = "../html/secondary-popup.html";
+const START_SCREEN_SHOTTER_MESSAGE = "start_screen_shotter";
 
+function getTab(callback){
+  var tab
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    callback(tabs);
+  });
+};
+
+
+function CaptureWork(){
+
+       chrome.tabs.getSelected(null,(tab) =>{
+
+        chrome.tabs.captureVisibleTab(tab.windowId, {format: 'png'}, (image) => {
+          // image is base64
+          crop(image, request.area, request.dpr, true, 'png', (cropped) => {
+            sendResponse({message: 'image', image: cropped})
+          })
+
+        })
+
+
+       })
+
+}
+
+
+function injectTab(tabs){
+  var tab = tabs[0];
+  chrome.tabs.sendMessage(tab.id, {message: 'init'}, (res) => {
+    if (res) {
+      clearTimeout(timeout)
+    }
+  })
+
+var timeout = setTimeout(() => {
+    chrome.tabs.insertCSS(tab.id, {file: 'css/jquery.Jcrop.min.css', runAt: 'document_start'})
+    chrome.tabs.insertCSS(tab.id, {file: 'css/cropCSS.css', runAt: 'document_start'})
+    chrome.tabs.executeScript(tab.id, {file: 'scripts/jquery/jquery-3.4.1.min.js', runAt: 'document_start'})
+    chrome.tabs.executeScript(tab.id, {file: 'scripts/jquery/jquery.Jcrop.min.js', runAt: 'document_start'})
+    chrome.tabs.executeScript(tab.id, {file: 'scripts/screen-shotter.js', runAt: 'document_start'})
+
+    setTimeout(() => {
+        chrome.tabs.sendMessage(tab.id, {message: 'init'})
+      }, 1000)
+    }, 100)
+};
 
 
 /* Messages that trigger actions that must be done by background.js because most chrome APIs can't be used by content scripts.
@@ -21,12 +68,105 @@ chrome.runtime.onMessage.addListener(
       case NOTIFY_OCR_EXECUTED_MESSAGE:
         onOcrExecuted(parseInt(request.statusCode), request.text);
         break;
+
+      case "capture":
+        chrome.tabs.getSelected(null,(tab) =>{
+
+          chrome.tabs.captureVisibleTab(tab.windowId, {format: 'png'}, (image) => {
+            // image is base64
+            crop(image, request.area, true, true, 'png', (cropped) => {
+              // sendResponse( cropped)
+              chrome.tabs.sendMessage(tab.id,
+                {
+                  message: 'CROPPED_IMAGE',
+                  croppedImage: cropped
+                })
+            })
+  
+          })
+  
+  
+         })
+      //capture();
+        break;
+      
+      case "popup_init":
+       getTab(injectTab);
+       break;
+
+//end new      
+    case "active":
+      return true;
+
     }
 
     // Notify sender that message was recieved.
-    sendResponse(request.message)
+//   sendResponse({'active': active})
   }
 );
+
+
+
+
+function inject(tab) {
+
+  chrome.tabs.sendMessage(tab.id, {message: 'init'}, (res) => {
+      if (res) {
+        clearTimeout(timeout)
+      }
+    })
+
+  var timeout = setTimeout(() => {
+      chrome.tabs.insertCSS(tab.id, {file: 'css/jquery.Jcrop.min.css', runAt: 'document_start'})
+      chrome.tabs.insertCSS(tab.id, {file: 'css/cropCSS.css', runAt: 'document_start'})
+      chrome.tabs.executeScript(tab.id, {file: 'scripts/jquery/jquery-3.4.1.min.js', runAt: 'document_start'})
+      chrome.tabs.executeScript(tab.id, {file: 'scripts/jquery/jquery.Jcrop.min.js', runAt: 'document_start'})
+      chrome.tabs.executeScript(tab.id, {file: 'scripts/screen-shotter.js', runAt: 'document_start'})
+
+      setTimeout(() => {
+          chrome.tabs.sendMessage(tab.id, {message: 'init'})
+        }, 100)
+      }, 100)
+
+}
+
+
+        
+        function crop (image, area, dpr, preserve, format, done) {
+          var top = area.y * dpr
+          var left = area.x * dpr
+          var width = area.w * dpr
+          var height = area.h * dpr
+          var w = (dpr !== 1 && preserve) ? width : area.w
+          var h = (dpr !== 1 && preserve) ? height : area.h
+      
+          var canvas = null
+          if (!canvas) {
+          canvas = document.createElement('canvas')
+          document.body.appendChild(canvas)
+          }
+          canvas.width = w
+          canvas.height = h
+        
+          var img = new Image()
+          img.onload = () => {
+          var context = canvas.getContext('2d')
+          context.drawImage(img,
+              left, top,
+              width, height,
+              0, 0,
+              w, h
+          )
+      
+          var cropped = canvas.toDataURL(`image/${format}`)
+          done(cropped)
+          }
+          img.src = image
+          console.log(img.src);
+        }
+
+
+
 
 // Executes when the OCR Lambda funciton returns.
 function onOcrExecuted(statusCode, recognizedText) {
@@ -57,4 +197,59 @@ function createNotification(message) {
     title: "CodeCap",
     message: message
   });
+}
+
+
+function fullCapture(){
+  var screenshot = {
+    content : document.createElement("canvas"),
+    data : '',
+  
+    init : function() {
+      this.initEvents();
+    },
+    
+    saveScreenshot : function() {
+      var image = new Image();
+      image.onload = function() {
+        var canvas = screenshot.content;
+        canvas.width = image.width;
+        canvas.height = image.height;
+        var context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0);
+  
+        // save the image
+        var link = document.createElement('a');
+        link.download = "download.png";
+        link.href = screenshot.content.toDataURL();
+        link.click();
+        screenshot.data = '';
+      };
+      image.src = screenshot.data; 
+    },
+    
+    initEvents : function() {
+        chrome.tabs.captureVisibleTab(null, {
+          format : "png",
+          quality : 100
+        }, function(data) {
+          screenshot.data = data;
+          
+          // send an alert message to webpage
+          chrome.tabs.query({
+            active : true,
+            currentWindow : true
+          }, function(tabs) {
+
+                screenshot.saveScreenshot();
+              
+
+
+          }); 
+  
+        });
+    }
+  };
+  
+  screenshot.init();
 }
--- scripts/initial-popup.js
+++ scripts/initial-popup.js
@@ -6,18 +6,27 @@ const START_SCREEN_SHOTTER_MESSAGE = "start_screen_shotter";
 
 // Set listener for upload button.
 var uploadButton = document.getElementById("upload_button");
-uploadButton.onclick = function (element) {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": START_FILE_UPLOADER_MESSAGE });
-    });
-};
-
+if (uploadButton){
+    uploadButton.onclick = function (element) {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, { "message": START_FILE_UPLOADER_MESSAGE });
+        });
+    };
+}
 // Set listener for screenshot button.
+
+
+
+
+
 var screenshotButton = document.getElementById("screenshot_button");
-screenshotButton.onclick = function (element) {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": START_SCREEN_SHOTTER_MESSAGE });
-    });
-};
+    screenshotButton.onclick = function (element) {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.runtime.sendMessage({message:"popup_init", tabID: activeTab.id})
+
+        });
+    }
+
+
